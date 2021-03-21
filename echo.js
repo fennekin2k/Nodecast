8a611bc14a0987f5425b023d4c67e474040a870b
fennekin2k <Anto96WS@Yandex.ru>
2021-03-21 11:13:26 +0300

    two-sided echo-server

--- echo.js
+++ echo.js
@@ -1,4 +1,8 @@
 const http =require('http');
+const url =require('url');
+
+
+let tbuff =Buffer.from([]);
 
 http.createServer((request, response) =>{
 	
@@ -8,17 +12,33 @@ http.createServer((request, response) =>{
 	response.setHeader('Access-Control-Allow-Origin', '*');
 	response.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
 	
-	response.writeHead(200, { 'content-Type': 'audio/webm' });
-	
-	console.log('Request received. Waiting for data');
-	
-	request.on('data', chunk => { // echo
-		console.log('Data chunk received: ', chunk);
-		response.write(chunk);
-	});
-	
-	request.on('end', () => {
-		console.log('Data stream ended');
-		response.end(null);
-	});
-}).listen(10225)
+	let pathname =url.parse(request.url).pathname;
+	switch(pathname){
+		case '/talk':
+			
+			response.writeHead(200);
+			
+			console.log('Request received. Waiting for data');
+			
+			request.on('data', chunk => { // echo
+				tbuff =chunk;
+				console.log('Data chunk received: ', chunk);
+			});
+			
+			request.on('end', () => {
+				console.log('Data stream ended');
+				response.end(null);
+			});
+			
+		break;
+		case '/listen':
+			
+			response.writeHead(200, { 'content-Type': 'audio/webm' });
+			response.end(tbuff);
+			
+		break;
+		default:
+			response.end('Error 404');
+		break;
+	}
+}).listen(8080);
